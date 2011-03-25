5d249e7e2181d8bfa196a109ef6697c4133b2427
Forest Basford <forest@janrain.com>
2011-03-25 09:34:50 -0700

    Cleaned up output to match old token php.

--- php/janrain-engage-php-sdk-0.1/rpx-token-url.php
+++ php/janrain-engage-php-sdk-0.1/rpx-token-url.php
@@ -3,9 +3,9 @@
  *Below is a very simple PHP 5 script that implements an Engage token URL to collect and output the results from auth_info.
  *The code below assumes you have the CURL HTTP fetching library with SSL and PHP JSON support.
  */
-
+ob_start();
 require_once('engage.lib.php');
-$the_output = '';
+$the_output_array = array();
 
 /**
  *For a production script it would be better to include the apiKey in from a file outside the web root to enhance security.
@@ -20,14 +20,20 @@ $result = engage_auth_info($api_key, $token, $format, $extended);
 if ($result === false) {
 	$errors = engage_get_errors();
 	foreach ($errors as $error=>$label) {
-		$the_output .= '<br />Error: '.$error;
+		$the_output_array[] = 'Error: '.$error;
 	}
 } else {
 	$array_out = true;
 	$auth_info_array = engage_parse_result($result, $format, $array_out);
-	$the_output .= print_r($auth_info_array, true);
+	$the_output_array = print_r($auth_info_array, true);
 }
 
+$the_buffer = ob_get_contents();
+if (!empty($the_buffer)) {
+	$the_output_array[] = 'Buffer: '.$the_buffer;
+}
+$the_output = implode("\n", $the_output_array);
+ob_end_clean();
 ?>
 <html>
 	<head>
@@ -39,4 +45,3 @@ if ($result === false) {
 		</pre>
 	</body>
 </html>
-
