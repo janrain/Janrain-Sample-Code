5da0ba9b27513ee6d54bf502caaec08bb0e6522d
Travis Mitchell <tcmitche@users.noreply.github.com>
2016-05-24 15:26:48 -0700

    Made node example more consistent.

--- signed-header-requests/signed-request.js
+++ signed-header-requests/signed-request.js
@@ -2,10 +2,10 @@
 const crypto = require('crypto');
 
 function makeSignedAuthHeader(endpoint, params, datetime, client_id, secret) {
-  const hmac = crypto.createHmac('sha1', secret)
   const paramKeys = Object.keys(params).sort()
   const paramString = paramKeys.map((k) => `${k}=${params[k]}`).join('\n')
   const stringToSign = `${endpoint}\n${datetime}\n${paramString}\n`
+  const hmac = crypto.createHmac('sha1', secret)
   hmac.update(stringToSign)
   return { Authorization: `Signature ${client_id}:${hmac.digest('base64')}` }
 }
